a77bb823b4c0b75f107b995a656335fdcdcb80f6
Pedro Antonio Martinez González <pedro.martinez@overclockers.com.mx>
2025-01-27 15:51:43 -0600

    4to commit

--- downloader.ps1
+++ downloader.ps1
@@ -1,6 +1,9 @@
 $path = $pwd | Select -ExpandProperty Path
 $client = new-object System.Net.WebClient
 
-New-Item -Path "$path\Wallpaper" -ItemType Directory
+Invoke-WebRequest -URI 'https://github.com/PedroPAMG/WallpaperSeguridad/raw/refs/heads/main/Wallpaper.zip' -OutFile 'Wallpaper.zip'
+Expand-Archive "$path\Wallpaper.zip" -DestinationPath "$path"
 
+Invoke-WebRequest -URI 'https://raw.githubusercontent.com/PedroPAMG/WallpaperSeguridad/refs/heads/main/wallpaper.ps1' -OutFile 'wallpaper.ps1'
 
+Remove-Item -Path "$path\Wallpaper.zip"
